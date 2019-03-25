a45de6323b3a2814ea619c0ff166f3fe168f3bbd
Basile Dura <basile.dura@polytechnique.edu>
2019-03-25 18:46:58 -0400

    Added a plot_ppls in utils.

--- utils.py
+++ utils.py
@@ -40,8 +40,13 @@ def get_times(folder):
 def plot(train, valid, name, save=True, log_scale=False):
     plt.figure()
 
-    plt.plot(train, label='train')
-    plt.plot(valid, label='validation')
+    if isinstance(train, list):
+        for t, v, n in zip(train, valid, name):
+            plt.plot(t, label='{} train'.format(n))
+            plt.plot(v, label='{} validation'.format(n))
+    else:
+        plt.plot(train, label='train')
+        plt.plot(valid, label='validation')
 
     plt.legend()
 
@@ -64,10 +69,16 @@ def plot(train, valid, name, save=True, log_scale=False):
 def plot_time(train, valid, times, name, save=True, log_scale=False):
     plt.figure()
 
-    hours = times / 3600
+    if isinstance(train, list):
+        hours = [time / 3600 for time in times]
+        for t, v, h, n in zip(train, valid, hours, name):
+            plt.plot(h, t, label='{} train'.format(n))
+            plt.plot(h, v, label='{} validation'.format(n))
+    else:
+        hours = times / 3600
 
-    plt.plot(hours, train, label='train')
-    plt.plot(hours, valid, label='validation')
+        plt.plot(hours, train, label='train')
+        plt.plot(hours, valid, label='validation')
 
     plt.legend()
 
@@ -98,6 +109,34 @@ def plot_ppl(name, folder, save=True, log_scale=False, time=False):
         plot_time(curves['train_ppls'], curves['val_ppls'], times, name + '-time', save, log_scale)
     else:
         plot(curves['train_ppls'], curves['val_ppls'], name, save, log_scale)
+
+
+def plot_ppls(experiments, save=True, log_scale=False, time=False):
+
+    folders = [e['folder'] for e in experiments]
+    names = [e['name'] for e in experiments]
+
+    curves = [get_curves(folder) for folder in folders]
+
+    times = [np.cumsum(get_times(folder)) for folder in folders]
+
+    if time:
+        plot_time(
+            [curve['train_ppls'] for curve in curves],
+            [curve['val_ppls'] for curve in curves],
+            times,
+            names,
+            save,
+            log_scale
+        )
+    else:
+        plot(
+            [curve['train_ppls'] for curve in curves],
+            [curve['val_ppls'] for curve in curves],
+            names,
+            save,
+            log_scale
+        )
 
 
 def load_weights(folder):
